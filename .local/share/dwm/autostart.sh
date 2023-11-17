a1f0d3e49850506e688173930b85df9599eb13d5
StarryMohannad <73769579+StarryMohannad@users.noreply.github.com>
2023-11-17 11:15:12 +0300

    Ahhhhhhhhhhh

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -9,4 +9,7 @@ setsid -f mpDris2 # MPD Dris Compatiblity
 
 setsid -f emacs --daemon # Emacs Daemon
 
+setsid -f "$DWM"/keepassxc.sh
+
 xwallpaper --stretch "$DWM"/wallpaper.png # Sets the Wallpaper
+setsid -f mpv --no-video "$HOME"/.local/share/sounds/startup.mp3
