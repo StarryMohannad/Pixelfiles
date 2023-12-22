a534d32ffd8839ccf45e85c7e836fd4fc2c9e22f
StarryMohannad <73769579+StarryMohannad@users.noreply.github.com>
2023-12-22 18:11:37 +0300

    More Edits for the README.org('s)

--- src/dwmblocks/blocks.def.h
+++ src/dwmblocks/blocks.def.h
@@ -1,10 +1,10 @@
 static const Block blocks[] = {
-{ "  ", "~/.local/share/dwm/blocks/mus", 1, 0},
-{ "󱐋  ", "~/.local/share/dwm/blocks/bat", 1, 0},
+{ "  ", "~/.local/share/dwm/blocks/mus", 1, 3},
+{ "󱐋  ", "~/.local/share/dwm/blocks/bat", 1, 2},
 { "󰕾  ", "~/.local/share/dwm/blocks/vol", 1, 0},
-{ "",    "~/.local/share/dwm/blocks/wtr", 1, 0},
-{ "󰸗  ", "~/.local/share/dwm/blocks/cal", 1, 0},
-{ "  ", "~/.local/share/dwm/blocks/tim", 1, 0}, };
+{ "",    "~/.local/share/dwm/blocks/wtr", 1, 5},
+{ "󰸗  ", "~/.local/share/dwm/blocks/cal", 1, 5},
+{ "  ", "~/.local/share/dwm/blocks/tim", 1, 1}, };
 
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
