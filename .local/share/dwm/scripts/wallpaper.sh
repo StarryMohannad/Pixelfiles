eb5452648c6466e65f37d4a110788be134d6231a
StarryMohannad <73769579+StarryMohannad@users.noreply.github.com>
2024-01-20 13:15:31 +0300

    typo :P

--- .local/share/dwm/scripts/wallpaper.sh
+++ .local/share/dwm/scripts/wallpaper.sh
@@ -6,5 +6,5 @@ wpg -a "$HOME"/.local/share/wallpapers/"$WallpaperChoice"
 wpg -s "$HOME"/.local/share/wallpapers/"$WallpaperChoice"
 pywal-discord -p "$HOME"/.config/Vencord/themes
 xdotool key Super+F5
-cp -r "$HOME"/.cache/wall/colors.css /tmp/pywal-pixelfiles-colors.css
-cp -r "$HOME"/.cache/wall/colors.scss /tmp/pywal-pixelfiles-colors.scss
+cp -r "$HOME"/.cache/wal/colors.css /tmp/pywal-pixelfiles-colors.css
+cp -r "$HOME"/.cache/wal/colors.scss /tmp/pywal-pixelfiles-colors.scss
