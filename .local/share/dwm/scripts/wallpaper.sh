a88ac2124287fb3fdaa07c68d5094db856855ffd
StarryMohannad <73769579+StarryMohannad@users.noreply.github.com>
2024-01-20 13:14:49 +0300

    Pywal Stylus ??

--- .local/share/dwm/scripts/wallpaper.sh
+++ .local/share/dwm/scripts/wallpaper.sh
@@ -6,3 +6,5 @@ wpg -a "$HOME"/.local/share/wallpapers/"$WallpaperChoice"
 wpg -s "$HOME"/.local/share/wallpapers/"$WallpaperChoice"
 pywal-discord -p "$HOME"/.config/Vencord/themes
 xdotool key Super+F5
+cp -r "$HOME"/.cache/wall/colors.css /tmp/pywal-pixelfiles-colors.css
+cp -r "$HOME"/.cache/wall/colors.scss /tmp/pywal-pixelfiles-colors.scss
